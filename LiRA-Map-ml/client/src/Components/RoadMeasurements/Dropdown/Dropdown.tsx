85cbcf02b6c897e6b2bdf280cb7894df4ad1e777
root <root@LAPTOP-S4AU0NIQ.localdomain>
2022-10-08 18:46:41 +0200

    changing measurement popup and enabling search in list

--- LiRA-Map-ml/client/src/Components/RoadMeasurements/Dropdown/Dropdown.tsx
+++ LiRA-Map-ml/client/src/Components/RoadMeasurements/Dropdown/Dropdown.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 type DropDownProps = {
+  //measurements: DataType[];
   measurements: string[];
   showDropDown: boolean;
   toggleDropDown: Function;
