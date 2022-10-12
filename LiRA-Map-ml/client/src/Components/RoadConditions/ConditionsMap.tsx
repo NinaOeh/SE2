ce628cc1ec92f4d8c17e5a6ef179e5a14f49149f
eliotu <eliot.ullmo@epfl.ch>
2022-10-12 09:52:30 +0200

    back to the merge of nina

--- LiRA-Map-ml/client/src/Components/RoadConditions/ConditionsMap.tsx
+++ LiRA-Map-ml/client/src/Components/RoadConditions/ConditionsMap.tsx
@@ -1,5 +1,5 @@
 
-import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FC, useCallback, useRef } from "react";
 import { ChartData } from "chart.js";
 import { Palette } from "react-leaflet-hotline";
 
@@ -14,10 +14,7 @@ import { ConditionType } from "../../models/graph";
 import { Condition } from "../../models/path";
 
 import { getConditions } from "../../queries/conditions";
-import { filter } from "d3";
-import createPopup from "../createPopup";
-import FilteringSelector from "./OptionsFiltering"
-import { FilteringOptions } from "../../models/models";
+
 
 interface Props {
     type: ConditionType;
@@ -33,60 +30,25 @@ const ConditionsMap: FC<Props> = ( { type, palette, setPalette, setWayData } ) =
     const ref = useRef(null);
     const [width, _] = useSize(ref)
 
-    
-    const onClick = useCallback((way_id: string, way_length: number,filter:number) => {
-
+    const onClick = useCallback( (way_id: string, way_length: number) => {
         getConditions( way_id, name, (wc: Condition[]) => {
-            const max = wc.reduce((prev, current) => (prev.value > current.value) ? prev : current).value
-            console.log("maximum value:",max);
-            console.log("the filter is:",filter);
-            if(max>filter){
-
-           
-                setWayData( {
-                    labels: wc.map( p => p.way_dist * way_length ),
-                    datasets: [ {
-                        type: 'line' as const,
-                        label: way_id,
-                        borderColor: 'rgb(160,32,240)',
-                        borderWidth: 2,
-                        fill: false,
-                        tension: 0.1,
-                        data: wc.map( p => p.value ),
-                    } ]
-                } )
-
-            }
-            else{
-               
-                setWayData( {
-                    labels: [],
-                    datasets: [ {
-                        type: 'line' as const,
-                        label: way_id,
-                        borderColor: 'rgb(160,32,240)',
-                        borderWidth: 2,
-                        fill: false,
-                        tension: 0.1,
-                        data: [],
-                    } ]
-                } )
-                const popup=createPopup();
-                popup( {
-                    icon: "warning",
-                    title: `This trip doesn't have any value with the ira wanted   `,
-                    toast: true
-                } );
-
-            }
-           
+            setWayData( {
+                labels: wc.map( p => p.way_dist * way_length ),
+                datasets: [ {
+                    type: 'line' as const,
+                    label: way_id,
+                    borderColor: 'rgb(255, 99, 132)',
+                    borderWidth: 2,
+                    fill: false,
+                    tension: 0.1,
+                    data: wc.map( p => p.value ),
+                } ]
+            } )
         } )
-    },[])
-
+    }, [] )
 
     return (
         <div className="road-conditions-map" ref={ref}>
-
             <PaletteEditor 
                 defaultPalette={RENDERER_PALETTE}
                 width={width}
@@ -94,8 +56,7 @@ const ConditionsMap: FC<Props> = ( { type, palette, setPalette, setWayData } ) =
                 onChange={setPalette} />
 
             <MapWrapper>
-                <Ways palette={palette} type={name} onClick={onClick}  />
-
+                <Ways palette={palette} type={name} onClick={onClick}/>
             </MapWrapper>
         </div>
     )
--- LiRA-Map-ml/client/src/Components/RoadConditions/Ways.tsx
+++ LiRA-Map-ml/client/src/Components/RoadConditions/Ways.tsx
@@ -1,43 +1,26 @@
 
-import { latLng, map } from 'Leaflet.MultiOptionsPolyline';
-import { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { TRGB } from 'react-gradient-hook/lib/types';
-import { Tooltip } from 'react-leaflet';
 import { HotlineOptions } from 'react-leaflet-hotline';
 import { HotlineEventHandlers } from 'react-leaflet-hotline/lib/types';
 import { useGraph } from '../../context/GraphContext';
-import { FilteringOptions } from '../../models/models';
 import { WaysConditions } from '../../models/path';
 import { getWaysConditions } from '../../queries/conditions';
-import createPopup from '../createPopup';
 import useZoom from '../Map/Hooks/useZoom';
 import DistHotline from '../Map/Renderers/DistHotline';
-import FilteringSelector from './OptionsFiltering';
 
 interface IWays {
     palette: TRGB[]
     type: string;
-    onClick?: (way_id: string, way_length: number,filter:number) => void;
+    onClick?: (way_id: string, way_length: number) => void;
 }
 
 const Ways: FC<IWays> = ( { palette, type, onClick } ) => {
+    
     const zoom = useZoom();
     const { minY, maxY } = useGraph()
 
     const [ways, setWays] = useState<WaysConditions>()
-    const [count, setCount] = useState(0);
-
-    const onChange=({search}: FilteringOptions) =>{
-        const number=Number(search)
-       
-        if(!isNaN(number)){
-            setCount(number);
-        }
-     
-
-
-    }
-
 
     const options = useMemo<HotlineOptions>( () => ({
         palette, min: minY, max: maxY
@@ -45,27 +28,16 @@ const Ways: FC<IWays> = ( { palette, type, onClick } ) => {
 
     const handlers = useMemo<HotlineEventHandlers>( () => ({
         click: (_, i) => {
-           /**  if(filter){
-                const popup=createPopup();
-                popup( {
-                    icon: "warning",
-                    title: `This trip doesn't have any value with the ira wanted   `,
-                    toast: true
-                } );
-        }**/
-            console.log("there is a problem");
             if ( ways && onClick )
-                onClick(ways.way_ids[i], ways.way_lengths[i],count)
+                onClick(ways.way_ids[i], ways.way_lengths[i])
         },
-     
-
-
-    }), [count,ways] )
+    }), [ways] )
 
     useEffect( () => {
         if ( zoom === undefined ) return;
         const z = Math.max(0, zoom - 12)
         getWaysConditions(type, z, (data: WaysConditions) => {
+            console.log(data)
             setWays( data )
         } )
     }, [zoom] )
@@ -73,24 +45,15 @@ const Ways: FC<IWays> = ( { palette, type, onClick } ) => {
     return (
         <>
         { ways 
-            ? <><DistHotline
-                    way_ids={ways.way_ids}
-                    geometry={ways.geometry}
-                    conditions={ways.conditions}
-                    options={options}
-                    eventHandlers={handlers}
-                    filter={count} /><Tooltip> xdn </Tooltip></>
-
+            ? <DistHotline 
+                way_ids={ways.way_ids}
+                geometry={ways.geometry}
+                conditions={ways.conditions} 
+                options={options} 
+                eventHandlers={handlers}/> 
             : null 
         }
-
-        <div className='toolbar-wrapper'>
-
-            <FilteringSelector onChange={onChange}/>
-
-        </div>
         </>
-
     )
 }
 
