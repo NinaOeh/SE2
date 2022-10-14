0bc54f6a9c810a53e5493935bd48f806fcde93f9
Andreasbirch <andreasbirch1998@hotmail.com>
2022-10-14 03:18:06 +0200

    Different solutions to fetching roadnames

--- LiRA-Map-ml/server-nest/src/rides/rides.service.ts
+++ LiRA-Map-ml/server-nest/src/rides/rides.service.ts
@@ -5,6 +5,7 @@ import { Knex } from 'knex';
 
 import { RideMeta } from './models.rides';
 import { BoundedPath, PointData } from 'src/models';
+const to_knex = require('postgresql-to-knex')
 
 @Injectable()
 export class RidesService 
@@ -12,8 +13,70 @@ export class RidesService
     constructor(@InjectConnection('lira-main') private readonly knex: Knex) {}
 
     async getRides(): Promise<RideMeta[]>
-    {   let knex = this.knex;
-        return await this.knex
+    {   //let knex = this.knex;
+        console.log("Starting knex thingy")
+        // console.log(to_knex('with wayPoints AS (SELECT * FROM public."MapReferences" WHERE "wayPointName" IS NOT NULL AND "wayPointName" IS NOT NULL), measurementsWith AS (SELECT DISTINCT ON ("FK_Trip") "MeasurementId","FK_Trip", "wayPointName" FROM public."Measurements" JOIN wayPoints ON "MeasurementId" = "FK_MeasurementId" ORDER BY "FK_Trip" ASC) SELECT "Trips".*, "wayPointName" FROM public."Trips" JOIN measurementsWith ON "TripId" = "FK_Trip"'))
+        console.log("Done with knex thingy")
+
+        //Winger den med with
+        return await 
+        this.knex.with('waypoints_alias', this.knex.select('*').from('MapReferences').whereRaw('"wayPointName" IS NOT NULL'))
+        .with('measurements_alias', this.knex.select('MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements').join('waypoints_alias', function() {
+            this.on('MeasurementId', '=', 'FK_MeasurementId')
+        }).orderBy('FK_Trip', 'asc'))
+        .select('Trips.*', 'wayPointName').from('Trips').join('measurements_alias', function() {
+            this.on('TripId', '=', 'FK_Trip')
+        });
+        
+        //Det her virker vidst - skal have tilføjet et where not ''
+        return await 
+        this.knex.with('waypoints_alias', this.knex.select('*').from('MapReferences').whereRaw('"wayPointName" IS NOT NULL'))
+        .select('MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements').join('waypoints_alias', function() {
+            this.on('MeasurementId', '=', 'FK_MeasurementId')
+        }).orderBy('FK_Trip', 'asc');
+        // .select('Trips.*', 'wayPointName').from('Trips').join('measurements_alias', function() {
+        //     this.on('TripId', '=', 'FK_Trip')
+        // });
+
+        //Alt kombineret med thens
+        // return await this.knex.select('Trips.*', 'wayPointName').from('Trips')
+        // .join(((knex) => knex.select('FK_Trip', 'MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements')
+        //     .join(((knex) => knex.select('*').from('MapReferences').whereRaw('MapReferences.wayPointName IS NOT NULL').andWhereNot('MapReferences.wayPointName', '').as('wayPoints')), function() {
+        //     this.on('MeasurementId', '=', 'FK_MeasurementId')
+        // }).orderBy('FK_Trip', 'asc').as('measurementsJoinWayPoints'))
+        // , function() {
+        //     this.on('TripId', '=', 'FK_Trip')
+        // });
+
+
+        // const wayPoints = this.knex.select('*').from('MapReferences').whereRaw('MapReferences.wayPointName IS NOT NULL').andWhereNot('MapReferences.wayPointName', '').as('wayPoints');
+        // const measurementsJoinWayPoints = this.knex.select('FK_Trip', 'MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements').join(wayPoints, function() {
+        //     this.on('MeasurementId', '=', 'FK_MeasurementId')
+        // }).orderBy('FK_Trip', 'asc').as('measurementsJoinWayPoints');
+
+        // //Alt kombineret med consts
+        // return await this.knex.select('Trips.*', 'wayPointName').from('Trips').join(measurementsJoinWayPoints, function() {
+        //     this.on('TripId', '=', 'FK_Trip')
+        // });
+
+        // //Forsøg på at merge measurementsWith og wayPoints
+        // return await this.knex.select('FK_Trip', 'MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements').join((knex) => {(knex.select('*').from('MapReferences').whereRaw('MapReferences.wayPointName IS NOT NULL').andWhereNot('MapReferences.wayPointName', ''))}, function() {
+        //     this.on('MeasurementId', '=', 'FK_MeasurementId')
+        // }).orderBy('FK_Trip', 'asc');
+
+        // //Følgende svarer til "measurementsWith" delen
+        // return await this.knex.select('FK_Trip', 'MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip').from('Measurements').join('MapReferences', function() {
+        //     this.on('MeasurementId', '=', 'FK_MeasurementId')
+        // }).orderBy('FK_Trip', 'asc');
+        
+
+
+
+
+
+
+        // return await this.knex.with('waypoints', (knex) => {(knex.select('*').from('public.MapReferences').whereRaw('wayPointName IS NOT NULL').andWhereRaw("wayPointName IS NOT NULL"))}).with('measurementsWith', (knex) => {(knex.select('FK_Trip', 'MeasurementId', 'FK_Trip', 'wayPointName').distinctOn('FK_Trip'))})
+        // return await this.knex().with(`waypoints`, (knex) => { (knex.select(`*`, ).from(`public . MapReferences`).whereRaw("wayPointName IS NOT NULL").andWhereRaw("wayPointName IS NOT NULL")) }), `measurementswith`, (knex) => { (knex.select(knex.raw(`DISTINCT ON(FK_Trip) MeasurementId`),`FK_Trip`, `wayPointName`, ).from(`public . Measurements`).innerJoin(`waypoints`, function() { this.on("MeasurementId","=","FK_MeasurementId")}).orderByRaw(`FK_Trip ASC`)) }).select(`Trips.*`, `wayPointName`, ).from(`public . Trips`).innerJoin(`measurementswith`, function() { this.on("TripId","=","FK_Trip")})
             // .select( '*' )
             // .from( { public: 'Trips' } )
             // .where( 'TaskId', 1472 )
@@ -34,14 +97,23 @@ export class RidesService
             // .from( { public: 'Trips' } )
             // .whereNot( 'TaskId', 0 )
             // .orderBy('TaskId')
-            .distinct()
-            .select('MapReferences.wayPointName', 'Trips.*')
-            .from('MapReferences')
-            .whereNot('wayPointName', null)
-            .whereNot('wayPointName', '')
-            .innerJoin('Measurements', 'Measurements.MeasurementId', 'MapReferences.FK_MeasurementId')
-            .innerJoin('Trips', 'Trips.TripId', 'Measurements.FK_Trip')
-            .orderBy('wayPointName', 'asc')
+
+            // .raw('with wayPoints AS (SELECT * FROM public."MapReferences" WHERE "wayPointName" IS NOT NULL AND "wayPointName" != \'\'), measurementsWith AS (SELECT DISTINCT ON ("FK_Trip") "MeasurementId","FK_Trip", "wayPointName"FROM public."Measurements" JOIN wayPoints ON "MeasurementId" = "FK_MeasurementId"ORDER BY "FK_Trip" ASC) SELECT "Trips".*, "wayPointName" FROM public."Trips" JOIN measurementsWith ON "TripId" = "FK_Trip"')
+
+            /*
+            with wayPoints AS (
+                SELECT * FROM public."MapReferences" 
+                WHERE "wayPointName" IS NOT NULL AND "wayPointName" != ''
+            ), measurementsWith AS (
+                SELECT DISTINCT ON ("FK_Trip") "MeasurementId","FK_Trip", "wayPointName"
+                FROM public."Measurements" JOIN wayPoints ON "MeasurementId" = "FK_MeasurementId"
+                ORDER BY "FK_Trip" ASC
+            )
+
+            SELECT "Trips".*, "wayPointName" FROM public."Trips" JOIN measurementsWith ON "TripId" = "FK_Trip"
+            */ 
+            // .with('wayPoints', (knex) => {()})
+
     }
 
     async getRide( tripId: string, dbName: string ): Promise<BoundedPath>
